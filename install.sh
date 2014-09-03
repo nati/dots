c2999df49bbf748ac479b9f5df682f43cd7aa082
Nachi Ueno <nati.ueno@gmail.com>
2014-09-03 16:42:45 +0900

    added vim dir

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ ln -s $PWD/vim ~/.vim
 
 # need to add powerline configuraion
 # http://qiita.com/alpaca_taichou/items/ab70f914a6a577e25d70
-
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 sudo update-alternatives --set editor /usr/bin/vim.basic
