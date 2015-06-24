b8f72aa6bdafa21856a36390c9637216dc87060f
Nachi Ueno <nati.ueno@gmail.com>
2015-06-24 15:04:02 +0900

    Revert "update tmux config & remove neocomplete"
    This reverts commit 568fef6929584ae2de2ef6cc5b75d694f23bf468.

--- install.sh
+++ install.sh
@@ -15,11 +15,3 @@ ln -s $PWD/git_vimdiff.sh ~/git_vimdiff.sh
 # http://qiita.com/alpaca_taichou/items/ab70f914a6a577e25d70
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 sudo update-alternatives --set editor /usr/bin/vim.basic
-
-sudo apt-get update
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:pi-rho/dev
-sudo apt-get update
-sudo apt-get install -y tmux=1.9a-1~ppa1~t htop
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
